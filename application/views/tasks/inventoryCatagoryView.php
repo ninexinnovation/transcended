5f9d938111f3f3205b47423989a17e53d71ddfd3
rumesh udash <rumesh udash>
2016-04-07 02:12:59 +0545

    labeling and error in inventory view fixed

--- application/views/tasks/inventoryCatagoryView.php
+++ application/views/tasks/inventoryCatagoryView.php
@@ -11,11 +11,11 @@ $inventoryCatagory_data=$this->DataModel->getInventoryItemCatagoryId();
 	<div class="row" id="inventoryCatagory_data">
 	<?php
 		foreach ($inventoryCatagory_data as $inventoryCatagory) {
-			echo '<a class="col-md-4 col-lg-3 btn btn-default grid-btn" data-btn-type="task-result" data-view="#inventoryCatagoryViewModalLabel">';
-			echo '<i data-send="true" data-name="id">'.$inventoryCatagory->catagory_id.'</i>';
+			echo '<a class="col-md-4 col-lg-3 btn btn-default grid-btn" data-btn-type="task-result" data-view="#inventoryCatagoryViewModal">';
+			echo 'ID:<i data-send="true" data-name="id">'.$inventoryCatagory->catagory_id.'</i>';
 		
-			echo '<span>'.$inventoryCatagory->catagory_name.'</span>';
-			echo '<span>'.$inventoryCatagory->stiching_charge.'</span>';
+			echo '<span>Catagory Name : '.$inventoryCatagory->catagory_name.'</span>';
+			echo '<span>Charges : '.$inventoryCatagory->stiching_charge.'</span>';
 			echo '</a>';
 		}
 	?>
