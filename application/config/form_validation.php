4bee04b549bd2c1fb24d3448600077289ce54f32
rumesh udash <rumesh udash>
2016-04-22 11:24:45 +0545

    validation

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -1,8 +1,8 @@
 <?php //if (!defined('BASEPATH')) exit('No direct script access allowed');
-class form_validation extends CI_Form_validation {
+// class form_validation extends CI_Form_validation {
 	function alpha_dash_space($str)
 	{
 	    return ( ! preg_match("/^([-a-z_ ])+$/i", $str)) ? FALSE : TRUE;
 	}
-}
+// }
 ?>
